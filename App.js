8abab879938d88f8bcaa8c196bb8a17939d8a81c
Nathan Kramer <nathan.l.kramer@gmail.com>
2024-03-08 11:09:38 +1100

    Add reproducible example for bug report

--- App.js
+++ App.js
@@ -1,10 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import * as Calendar from "expo-calendar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  useEffect(() => {
+    async function requestPermissions() {
+      await Calendar.requestCalendarPermissionsAsync();
+    }
+
+    requestPermissions();
+  }, []);
+
+  const onClick = async () => {
+    const calendar = await Calendar.getDefaultCalendarAsync();
+
+    const recurrenceRule = {
+      daysOfTheWeek: [
+        { dayOfTheWeek: 2 },
+        { dayOfTheWeek: 3 },
+        { dayOfTheWeek: 6 },
+      ],
+      frequency: "WEEKLY",
+    };
+
+    const event = {
+      startDate: new Date(),
+      endDate: new Date(),
+      recurrenceRule,
+      title: "Test Calendar Event",
+    };
+
+    console.log("[Creating Event]\n", JSON.stringify(event, null, 2));
+    await Calendar.createEventAsync(calendar.id, event);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        The below button should create a recurring event in the device's
+        calendar.
+      </Text>
+      <Button title="Create" onPress={onClick} />
+
+      <Text>
+        After clicking, please go to the calendar app and note that the event is
+        not recurring.
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +54,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
